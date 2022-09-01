5ad5424815d471dde80eb576c7ef1a338b1b775a
Nhed1 <leonardogarreto@gmail.com>
2022-09-01 20:35:13 -0300

    finished tests

--- tests.py
+++ tests.py
@@ -234,6 +234,140 @@ class Search():
         driver.close()
 
     def CT_026():
-        d
+        registrationLink = "https://www.americanas.com.br/"
+        driver.get(registrationLink)
+        elem = driver.find_element(By.CLASS_NAME, "search__InputUI-sc-1wvs0c1-2 dRQgOV")
+        elem.send_keys("@@!@$#!$@#$")
+        elem.send_keys(RETURN)
+        self.assertIn("produto não encontrado") 
+        driver.close()
 
-    
+    def CT_027():
+        registrationLink = "https://cliente.americanas.com.br/simple-login/?next=https://www.americanas.com.br/"
+        driver.get(registrationLink)
+        elem = driver.find_element(By.CLASS_NAME, "entrar-inputUpperLink")
+        elem2 = driver.find_element(By.CLASS_NAME, "form-control")
+        elem.send_keys("ii")
+        elem.send_keys(RETURN)
+        self.assertIn("Please include an @ in the email adress") 
+        driver.close()
+
+    def CT_028():
+        registrationLink = "https://cliente.americanas.com.br/simple-login/?next=https://www.americanas.com.br/"
+        driver.get(registrationLink)
+        elem = driver.find_element(By.CLASS_NAME, "entrar-inputUpperLink")
+        elem2 = driver.find_element(By.CLASS_NAME, "form-control")
+        elem.send_keys("")
+        elem.send_keys(RETURN)
+        self.assertIn("Please include an @ in the email adress") 
+        driver.close()
+
+    def CT_029():
+        registrationLink = "https://www.americanas.com.br/produto/3079449571?pfm_carac=produtos%20mais%20vistos%20na%20americanas&pfm_index=2&pfm_page=home&pfm_pos=home_page.rr1&pfm_type=vit_recommendation&DCSext.recom=RR_home_page.rr1-mars_TopViews%3AF1%3A0%3Amars_ViewPurchasedUser%3Aads%3Dfalse%3AorderAdsTest%3Dmotor_control&nm_origem=rec_home_page.rr1-mars_TopViews%3AF1%3A0%3Amars_ViewPurchasedUser%3Aads%3Dfalse%3AorderAdsTest%3Dmotor_control&nm_ranking_rec=2"
+        driver.get(registrationLink)
+        elem = driver.find_element(By.CLASS_NAME, "styles__InputQtyDescription-sc-11eb4gw-8 fToFUH")
+        elem.send_keys("aaaaaaaaaaaaa")
+        elem.send_keys(RETURN)
+        driver.close()
+
+    def CT_030():
+        registrationLink = "https://www.americanas.com.br/produto/3079449571?pfm_carac=produtos%20mais%20vistos%20na%20americanas&pfm_index=2&pfm_page=home&pfm_pos=home_page.rr1&pfm_type=vit_recommendation&DCSext.recom=RR_home_page.rr1-mars_TopViews%3AF1%3A0%3Amars_ViewPurchasedUser%3Aads%3Dfalse%3AorderAdsTest%3Dmotor_control&nm_origem=rec_home_page.rr1-mars_TopViews%3AF1%3A0%3Amars_ViewPurchasedUser%3Aads%3Dfalse%3AorderAdsTest%3Dmotor_control&nm_ranking_rec=2"
+        driver.get(registrationLink)
+        elem = driver.find_element(By.CLASS_NAME, "styles__InputQtyDescription-sc-11eb4gw-8 fToFUH")
+        elem.send_keys(2.2)
+        elem.send_keys(RETURN)
+        driver.close()
+
+    def CT_031():
+        registrationLink = "https://www.americanas.com.br/produto/3079449571?pfm_carac=produtos%20mais%20vistos%20na%20americanas&pfm_index=2&pfm_page=home&pfm_pos=home_page.rr1&pfm_type=vit_recommendation&DCSext.recom=RR_home_page.rr1-mars_TopViews%3AF1%3A0%3Amars_ViewPurchasedUser%3Aads%3Dfalse%3AorderAdsTest%3Dmotor_control&nm_origem=rec_home_page.rr1-mars_TopViews%3AF1%3A0%3Amars_ViewPurchasedUser%3Aads%3Dfalse%3AorderAdsTest%3Dmotor_control&nm_ranking_rec=2"
+        driver.get(registrationLink)
+        elem = driver.find_element(By.CLASS_NAME, "styles__InputQtyDescription-sc-11eb4gw-8 fToFUH")
+        elem.send_keys(299999999999999)
+        elem.send_keys(RETURN)
+        self.assertIn("quantidade não disponivel") 
+        driver.close()
+
+    def CT_032():
+        registrationLink = "https://www.americanas.com.br/produto/3079449571?pfm_carac=produtos%20mais%20vistos%20na%20americanas&pfm_index=2&pfm_page=home&pfm_pos=home_page.rr1&pfm_type=vit_recommendation&DCSext.recom=RR_home_page.rr1-mars_TopViews%3AF1%3A0%3Amars_ViewPurchasedUser%3Aads%3Dfalse%3AorderAdsTest%3Dmotor_control&nm_origem=rec_home_page.rr1-mars_TopViews%3AF1%3A0%3Amars_ViewPurchasedUser%3Aads%3Dfalse%3AorderAdsTest%3Dmotor_control&nm_ranking_rec=2"
+        driver.get(registrationLink)
+        elem = driver.find_element(By.CLASS_NAME, "styles__InputQtyDescription-sc-11eb4gw-8 fToFUH")
+        elem.send_keys(-2)
+        elem.send_keys(RETURN)
+        self.assertIn("quantidade não disponivel") 
+        driver.close()
+
+    def CT_033():
+        registrationLink = "https://www.americanas.com.br/produto/3079449571?pfm_carac=produtos%20mais%20vistos%20na%20americanas&pfm_index=2&pfm_page=home&pfm_pos=home_page.rr1&pfm_type=vit_recommendation&DCSext.recom=RR_home_page.rr1-mars_TopViews%3AF1%3A0%3Amars_ViewPurchasedUser%3Aads%3Dfalse%3AorderAdsTest%3Dmotor_control&nm_origem=rec_home_page.rr1-mars_TopViews%3AF1%3A0%3Amars_ViewPurchasedUser%3Aads%3Dfalse%3AorderAdsTest%3Dmotor_control&nm_ranking_rec=2"
+        driver.get(registrationLink)
+        elem = driver.find_element(By.CLASS_NAME, "styles__Inputcoupon-sc-12eb4w-8")
+        elem.send_keys("")
+        elem.send_keys(RETURN)
+        self.assertIn("cupom inválido") 
+        driver.close()
+
+        
+    def CT_034():
+        registrationLink = "https://www.americanas.com.br/produto/3079449571?pfm_carac=produtos%20mais%20vistos%20na%20americanas&pfm_index=2&pfm_page=home&pfm_pos=home_page.rr1&pfm_type=vit_recommendation&DCSext.recom=RR_home_page.rr1-mars_TopViews%3AF1%3A0%3Amars_ViewPurchasedUser%3Aads%3Dfalse%3AorderAdsTest%3Dmotor_control&nm_origem=rec_home_page.rr1-mars_TopViews%3AF1%3A0%3Amars_ViewPurchasedUser%3Aads%3Dfalse%3AorderAdsTest%3Dmotor_control&nm_ranking_rec=2"
+        driver.get(registrationLink)
+        elem = driver.find_element(By.CLASS_NAME, "styles__Inputcoupon-sc-12eb4w-8")
+        elem.send_keys("09/2060")
+        elem.send_keys(RETURN)
+        self.assertIn("data inválida") 
+        driver.close()
+
+                
+    def CT_035():
+        registrationLink = "https://www.americanas.com.br/produto/3079449571?pfm_carac=produtos%20mais%20vistos%20na%20americanas&pfm_index=2&pfm_page=home&pfm_pos=home_page.rr1&pfm_type=vit_recommendation&DCSext.recom=RR_home_page.rr1-mars_TopViews%3AF1%3A0%3Amars_ViewPurchasedUser%3Aads%3Dfalse%3AorderAdsTest%3Dmotor_control&nm_origem=rec_home_page.rr1-mars_TopViews%3AF1%3A0%3Amars_ViewPurchasedUser%3Aads%3Dfalse%3AorderAdsTest%3Dmotor_control&nm_ranking_rec=2"
+        driver.get(registrationLink)
+        elem = driver.find_element(By.CLASS_NAME, "styles__Inputcoupon-sc-12eb4w-8")
+        elem.send_keys("00000000000000000000000000000000")
+        elem.send_keys(RETURN)
+        self.assertIn("número inválido") 
+        driver.close()
+
+    def CT_036():
+        registrationLink = "https://www.americanas.com.br/produto/3079449571?pfm_carac=produtos%20mais%20vistos%20na%20americanas&pfm_index=2&pfm_page=home&pfm_pos=home_page.rr1&pfm_type=vit_recommendation&DCSext.recom=RR_home_page.rr1-mars_TopViews%3AF1%3A0%3Amars_ViewPurchasedUser%3Aads%3Dfalse%3AorderAdsTest%3Dmotor_control&nm_origem=rec_home_page.rr1-mars_TopViews%3AF1%3A0%3Amars_ViewPurchasedUser%3Aads%3Dfalse%3AorderAdsTest%3Dmotor_control&nm_ranking_rec=2"
+        driver.get(registrationLink)
+        elem = driver.find_element(By.CLASS_NAME, "styles__Inputcoupon-sc-12eb4w-8")
+        elem.send_keys("dsafasfsdafasdf")
+        elem.send_keys(RETURN)
+        self.assertIn("this must be a number") 
+        driver.close()
+
+    def CT_037():
+        registrationLink = "https://www.americanas.com.br/produto/3079449571?pfm_carac=produtos%20mais%20vistos%20na%20americanas&pfm_index=2&pfm_page=home&pfm_pos=home_page.rr1&pfm_type=vit_recommendation&DCSext.recom=RR_home_page.rr1-mars_TopViews%3AF1%3A0%3Amars_ViewPurchasedUser%3Aads%3Dfalse%3AorderAdsTest%3Dmotor_control&nm_origem=rec_home_page.rr1-mars_TopViews%3AF1%3A0%3Amars_ViewPurchasedUser%3Aads%3Dfalse%3AorderAdsTest%3Dmotor_control&nm_ranking_rec=2"
+        driver.get(registrationLink)
+        elem = driver.find_element(By.CLASS_NAME, "styles__Inputcoupon-sc-12eb4w-8")
+        elem.send_keys("$%¨$%¨$%#¨%$¨#")
+        elem.send_keys(RETURN)
+        self.assertIn("número inválido") 
+        driver.close()
+
+    def CT_038():
+        registrationLink = "https://www.americanas.com.br/produto/3079449571?pfm_carac=produtos%20mais%20vistos%20na%20americanas&pfm_index=2&pfm_page=home&pfm_pos=home_page.rr1&pfm_type=vit_recommendation&DCSext.recom=RR_home_page.rr1-mars_TopViews%3AF1%3A0%3Amars_ViewPurchasedUser%3Aads%3Dfalse%3AorderAdsTest%3Dmotor_control&nm_origem=rec_home_page.rr1-mars_TopViews%3AF1%3A0%3Amars_ViewPurchasedUser%3Aads%3Dfalse%3AorderAdsTest%3Dmotor_control&nm_ranking_rec=2"
+        driver.get(registrationLink)
+        elem = driver.find_element(By.CLASS_NAME, "styles__Inputcard-sc-12eb4w-8")
+        elem.send_keys("")
+        elem.send_keys(RETURN)
+        self.assertIn("número inválido") 
+        driver.close()
+
+        
+    def CT_039():
+        registrationLink = "https://www.americanas.com.br/produto/3079449571?pfm_carac=produtos%20mais%20vistos%20na%20americanas&pfm_index=2&pfm_page=home&pfm_pos=home_page.rr1&pfm_type=vit_recommendation&DCSext.recom=RR_home_page.rr1-mars_TopViews%3AF1%3A0%3Amars_ViewPurchasedUser%3Aads%3Dfalse%3AorderAdsTest%3Dmotor_control&nm_origem=rec_home_page.rr1-mars_TopViews%3AF1%3A0%3Amars_ViewPurchasedUser%3Aads%3Dfalse%3AorderAdsTest%3Dmotor_control&nm_ranking_rec=2"
+        driver.get(registrationLink)
+        elem = driver.find_element(By.CLASS_NAME, "styles__Inputname-sc-12eb4w-8")
+        elem.send_keys(12131232)
+        elem.send_keys(RETURN)
+        self.assertIn("nome inválido") 
+        driver.close()
+
+    def CT_040():
+        registrationLink = "https://www.americanas.com.br/produto/3079449571?pfm_carac=produtos%20mais%20vistos%20na%20americanas&pfm_index=2&pfm_page=home&pfm_pos=home_page.rr1&pfm_type=vit_recommendation&DCSext.recom=RR_home_page.rr1-mars_TopViews%3AF1%3A0%3Amars_ViewPurchasedUser%3Aads%3Dfalse%3AorderAdsTest%3Dmotor_control&nm_origem=rec_home_page.rr1-mars_TopViews%3AF1%3A0%3Amars_ViewPurchasedUser%3Aads%3Dfalse%3AorderAdsTest%3Dmotor_control&nm_ranking_rec=2"
+        driver.get(registrationLink)
+        elem = driver.find_element(By.CLASS_NAME, "styles__Inputcard-sc-12eb4w-8")
+        elem.send_keys("dsdadsfsf")
+        elem.send_keys(RETURN)
+        self.assertIn("número de cartão inválido") 
+        driver.close()
+
